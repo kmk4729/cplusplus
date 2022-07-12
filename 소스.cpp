204fecd8d768c6534b38d83dfadfae701a3f06c1
LAPTOP-8E6M58RP\USER <kmk4729@naver.com>
2022-07-12 16:40:13 +0900

    test code

---
+++ 소스.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	const char* str = "phrase";
+	cout << *str << endl;
+	str++;	
+	cout << *str << endl;
+	str++;
+	cout << *str << endl;
+	str++;
+	cout << *str << endl;
+	str++;
+
+}
