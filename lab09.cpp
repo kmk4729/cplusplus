092c07a96d2618d751cdf428d4c87d5527d7ed04
kmk4729 <53010625+kmk4729@users.noreply.github.com>
2022-07-15 19:55:10 +0900

    Update lab09.cpp

--- lab09.cpp
+++ lab09.cpp
@@ -143,3 +143,52 @@ int main() {
 		
 	}
 }
+
+
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int** buildTable(int n) { 
+	int** arr;
+	arr = (int**)malloc(sizeof(int*) * n);
+	arr[0] = (int*)malloc(sizeof(int) * n * n);
+	for (int i = 1; i < n; i++) {
+		arr[i] = arr[i - 1] + n;
+	}
+	return arr;
+}
+void make_identity_matrix(int** table, int n) { 
+	for (int row = 0; row < n; row++) {
+		for (int col = 0; col < n; col++) {
+			table[row][col] = 0;
+		}
+	}
+	for (int count = 0; count < n; count++) {
+		table[count][count] = 1;
+	}
+}
+void printTable(int** table, int n) { 
+	for (int row = 0; row < n; row++) {
+		for (int col = 0; col < n; col++) {
+			cout <<setw(4) << table[row][col] << " ";
+		}
+		cout << endl;
+	}
+
+}
+int main() {
+int n = 0;
+cout << "N을 입력하시오: ";
+cin >> n;
+if (n < 1) {
+cout << "\n행렬을 생성할 수 없습니다.\n" << endl;
+exit(EXIT_FAILURE);
+}
+int** table = buildTable(n);
+make_identity_matrix(table, n);
+printTable(table, n);
+free(table[0]);
+free(table);
+return 0;
+}
