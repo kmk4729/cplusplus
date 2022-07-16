77b1ad3f9bac2c4cf649d87b1ce30635fa442fa4
kmk4729 <53010625+kmk4729@users.noreply.github.com>
2022-07-16 21:01:57 +0900

    Update lab10.cpp

--- lab10.cpp
+++ lab10.cpp
@@ -36,3 +36,57 @@ int main() {
 	cout << pt2.getY() << endl;
 	return 0;
 }
+
+
+
+#include <string>
+#include <iostream>
+using namespace std;
+class Account {
+	//private: //멤버변수
+	string name;
+	string id;
+	double balance;
+public: // 멤버함수(method)
+// 3개의 생성자 구현
+	Account() {
+		name = "";
+		id = "";
+		balance = 0;
+	}; // 기본생성자. name : "", id : "", balance : 0 으로 초기화
+	Account(string _name, string _id) {
+		name = _name;
+		id = _id;
+		balance = 0;
+} // name, id 받아오고, balance는 0으로 초기화
+	Account(string _name, string _id, int _balance) :name(_name), id(_id)
+		, balance(_balance) {};// name, id , balance 받아와서 초기화, balance < 0 인 경우 0으로 초기화
+	void deposit(double _amt) { balance += _amt; }
+	bool withdraw(double _amt) {
+		if (balance - _amt < 0)
+			return false;
+		balance -= _amt;
+		return true;
+	}
+	void print() {
+		cout << name << ", " << id << ", " << balance << endl;
+	}
+};
+int main() {
+	Account ac1("배성호", "1002", 5000);
+	Account ac2;
+	ac2.print();
+	ac1.print();
+	int depo;
+	cout << "예금할 금액을 입력하세요 : ";
+	cin >> depo;
+	ac1.deposit(depo);
+	ac1.print(); // print() 함수로 ac1 출력
+	int wdrw;
+	cout << "출금할 금액을 입력하세요 : ";
+	cin >> wdrw;
+	if (!ac1.withdraw(wdrw))
+		cout << "잔액이 부족합니다." << endl;
+	ac1.print();
+	return 0;
+}
