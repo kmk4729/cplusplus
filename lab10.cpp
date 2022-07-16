0ada62aabf83a7c2b655f0436eebc9285cf22334
kmk4729 <53010625+kmk4729@users.noreply.github.com>
2022-07-16 22:38:45 +0900

    Update lab10.cpp

--- lab10.cpp
+++ lab10.cpp
@@ -233,3 +233,119 @@ int main() {
 	}
 	return 0;
 }
+
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Student {
+private:
+	string name;
+	int id;
+	string major;
+	vector<string> subject;
+	vector<char> grade;
+public:
+	Student():name("default"),id(0),major("depart") {}
+	Student(string _name, int _id, string _major) {
+		name = _name;
+		id = _id;
+		major = _major;
+	}
+	void setID(int _id) {
+		id = _id;
+	}
+	void setName(string _name) {
+		name = _name;
+	}
+	void setDpt(string _major) {
+		major = _major;
+	}
+	void print() {
+		cout << name << " " << id << " " << major<<endl;
+	}
+
+	void addGrade(string _subject, char _grade) {
+		grade.push_back(_grade);
+		subject.push_back(_subject);
+	}
+	void printGrades() {
+		for (int k = 0; k < (subject.size()); k++) {
+			cout << subject[k] << " " << grade[k] << endl;
+		}
+		cout << "GPA : " << getGPA() << endl;;
+	}
+	float getGPA() {
+		float sum = 0.0;
+		int length = grade.size();
+		for (auto elem : grade) {
+			switch (elem) {
+			case 'A':
+				sum += 4.0;
+				break;
+			case 'B':
+				sum += 3.0;
+				break;
+			case 'C':
+				sum += 2.0;
+				break;
+			case 'D':
+				sum += 1.0;
+				break;
+			case 'F':
+				sum += 0.0;
+				break;
+
+			}
+		}
+		return sum / (length);
+	}
+	void getYear(int year) {
+	int stuyear = id / 1000000;
+	switch (year-stuyear)
+		case 0: {
+		cout << "Freshman(1학년)" << endl;
+		break;
+		case 1:
+			cout << "Sophomore(2학년)" << endl;
+			break;
+		case 2:
+			cout << "Junior(3학년)" << endl;
+			break;
+		case 3:
+			cout << "Senior(4학년)" << endl;
+			break;
+		default:
+			cout << "About to graduate(5학년)" << endl;
+			break;
+	}
+	}
+};
+
+
+int main() {
+	Student Harry("Harry", 2017310973, "CS");
+	Harry.print();
+	Harry.getYear(2019);
+	Harry.addGrade("programming", 'A');
+	Harry.addGrade("Basic Circuit", 'B');
+	Harry.printGrades();
+	cout << "\n\n";
+	Student Ron;
+	Ron.print();
+	cout << "\n";
+	Ron.setName("Ron");
+	Ron.setID(2014103959);
+	Ron.setDpt("EE");
+	Ron.print();
+	Ron.getYear(2019);
+	Ron.addGrade("Computer Architecture", 'B');
+	Ron.addGrade("Maching Learning", 'B');
+	Ron.addGrade("Computer Vision", 'C');
+	Ron.printGrades();
+	cout << "\n\n";
+	return 0;
+	
+}
