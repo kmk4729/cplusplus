6a4adadc4b4c4870288fed8a1be9b952948a0eed
kmk4729 <53010625+kmk4729@users.noreply.github.com>
2022-07-16 21:39:26 +0900

    Update lab10.cpp

--- lab10.cpp
+++ lab10.cpp
@@ -145,3 +145,91 @@ int main() {
 	cout << "전공 :" << s1.getMajor() << endl;
 	return 0;
 }
+
+
+
+#include <iostream>
+using namespace std;
+
+class CStudent {
+private:
+	int id;
+	string name;
+	string major;
+public:
+	CStudent() :id(2018000000), name("홍길동"), major("컴퓨터공학과") {};
+	CStudent(int _id, string _name, string _major) :id(_id), name(_name), major(_major) {};
+	void setNumber(int num) {
+		id = num;
+}
+	void setName(string _name) {
+		name = _name;
+	}
+	void setMajor(string _major) {
+		major = _major;
+	}
+
+	void Display() {
+		cout << "학번 :" << id << endl 
+		<< "이름 :" << name << endl 
+		<< "전공 :" << major << endl<<endl;
+	}
+
+	int getNumber() {
+		return id;
+	}
+	string getName() {
+		return name;
+	}
+	string getMajor() {
+		return major;
+	}
+};
+
+int main() {
+	CStudent s[3]; // 3명의 학생정보를 저장할 class 배열
+	int inputNumber; // 키보드로 학번을 입력 받을 변수
+	string inputName, inputMajor; // 키보드로 이름, 전공을 입력 받을 변수
+	int length = 0; // 현재 입력된 학생의 수
+	while (1) { 
+		cout << length+1 << " 번째 학생 입력" << endl;
+		cout << "학번 : ";
+		cin >> inputNumber;
+		cout << "이름 : ";
+		cin >> inputName;
+		cout << "전공 : ";
+		cin >> inputMajor;
+
+		if (length != 0) {
+			int sum = 0;
+			for (int k = 0; k < length; k++) {
+				if (s[k].getNumber() == inputNumber) {
+					cout << "중복된 학번이 존재합니다" << endl;
+					sum++;
+				}
+			}
+			if (sum == 0) {
+				s[length].setNumber(inputNumber);
+				s[length].setName(inputName);
+				s[length].setMajor(inputMajor);
+				length++;
+			}
+		}
+		else {
+			s[length].setNumber(inputNumber);
+			s[length].setName(inputName);
+			s[length].setMajor(inputMajor);
+			length++;
+		}
+		if (length == 3) {
+			cout << "----모든입력이 완료되었습니다----" << endl;
+			for (int k = 0; k < 3; k++) {
+				cout << k + 1 << "학생정보" << endl;
+				s[k].Display();
+			}
+			break;
+
+		}
+	}
+	return 0;
+}
